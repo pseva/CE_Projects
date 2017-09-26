90a85552a83095dc7ea852780628399acf03e8af
pseva <30699845+pseva@users.noreply.github.com>
2017-09-26 13:13:24 -0700

    Add files via upload

---
+++ Q1-Q3 NPS GB.sql
@@ -0,0 +1,23 @@
+select 
+Store_Number,
+store_name as Store_Name,
+store_country_name as Country,
+Detractors, 
+Passives, 
+Promoters, 
+Total_Responses, 
+100 * Promoters / Total_Responses as Promoter_Pct,
+100 * Detractors / Total_Responses as Detractor_Pct,
+Promoter_Pct-Detractor_Pct as NPS_Score
+from 
+(select retail_store_id as Store_Number,
+sum(case when score_recomm < 4 then 1 end) as Detractors,
+sum(case when score_recomm = 4 then 1 end) as Passives,
+sum(case when score_recomm > 4 then 1 end) as Promoters,
+count(*) as Total_Responses
+from rtl_amr.p_geniusbarNPSfusion
+where score_recomm is not null
+and visit_date between '2016/09/25' and '2017/07/01'
+group by retail_store_id)A 
+join rtl_amr.d_storeattr 
+on (A.Store_Number = rtl_amr.d_storeattr.retail_store_id);
